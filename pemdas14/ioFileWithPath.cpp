de0118b3d2af801e5b9fb38c08221ba25c49c759
verss21 <arjunafrizal01@gmail.com>
2024-06-03 10:15:18 +0700

    loop untuk baris

--- pemdas14/ioFileWithPath.cpp
+++ pemdas14/ioFileWithPath.cpp
@@ -7,3 +7,24 @@ int main() {
 	string baris;
 	string NamaFile;
 
+	cout << "Masukan nama file : ";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+}

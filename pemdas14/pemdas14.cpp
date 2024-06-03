58549f2b64fceb45f53a18ed0a2c080c708c3b37
verss21 <arjunafrizal01@gmail.com>
2024-06-03 09:59:50 +0700

    membuka dan membaca file

--- pemdas14/pemdas14.cpp
+++ pemdas14/pemdas14.cpp
@@ -25,4 +25,22 @@ int main() {
 	}
 	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+
+	}
 }

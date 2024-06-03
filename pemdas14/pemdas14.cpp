7e7f853172a91fd98750024d1d251ea791164a9b
verss21 <arjunafrizal01@gmail.com>
2024-06-03 09:50:58 +0700

    loop untuk baris

--- pemdas14/pemdas14.cpp
+++ pemdas14/pemdas14.cpp
@@ -10,4 +10,17 @@ int main() {
 	ofstream outfile;
 	//menunjuk ke sebuah nama file
 	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		// menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
